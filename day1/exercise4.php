a724a7b040d7924a5ebac58371ded0ea2958b2f7
Lenny <astienlenny@gmail.com>
2023-06-13 16:01:42 +0200

    done

---
+++ day1/exercise4.php
@@ -0,0 +1,15 @@
+<?php
+$str = "Hello Worl";
+
+$i = 0;
+
+
+while( $i < $str )
+{
+  echo $str[$i] . ' ' ;
+  $i++; 
+}
+?>
+
+ 
+ 
---
+++ day1/exercise5.php
@@ -0,0 +1,12 @@
+<?php
+$str = "abcdefghijklmnopqrstuvwxyz";
+
+$i = 0;
+
+
+while( $i < $str )
+{
+  echo $str [$i +=1] ;
+  $i++;
+}
+?>
